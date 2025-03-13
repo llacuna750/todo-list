da7e963098cebbe3221e79090ba3cb5dc22d9a7a
jampasfredirect <jampasfredirect@gmail.com>
2025-03-13 21:35:34 +0800

    add gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    base: '/todo-list/', // Replace with your actual GitHub repo name
+});
