0bb10b50aa7fc976737012faacb044cf2241d84a
jampasfredirect <jampasfredirect@gmail.com>
2025-03-14 05:56:15 +0800

    Fix black screen issue

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     base: '/todo-list/', // Replace with your actual GitHub repo name
+    server: {
+    port: parseInt(process.env.VITE_PORT) || 5173,
+    },
+    build: {
+    outDir: 'dist',
+    }
 });
